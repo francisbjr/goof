5d64c241dd2724d0b84b813e6b84866ca640614a
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-02-02 22:00:17 -0500

    Write psuedo code for rock-paper-scissors.js file

---
+++ rock-paper-scissors.js
@@ -0,0 +1,9 @@
+//create 'getComputerChoice' function
+function getComputerChoice () {
+    //create an array with 'rock + paper + scissors'
+    let choice = ['rock', 'paper', 'scissors'];
+    //create a variable that will pick a random string out of the array
+    let random ;
+    //return the variable
+    return ;
+}
