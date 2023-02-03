2ca720caf601565ff048dde0ea1dcae89f2e6e42
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-02-02 22:31:20 -0500

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,7 +3,9 @@ function getComputerChoice () {
     //create an array with 'rock + paper + scissors'
     let choice = ['rock', 'paper', 'scissors'];
     //create a variable that will pick a random string out of the array
-    let random ;
+    let random = Math.floor(Math.random() * choice.length);
     //return the variable
-    return ;
+    return choice[random];
 }
+
+console.log(getComputerChoice());
