1e2acbd8b7596612220bdd920117b64a301779c4
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-02-10 10:00:39 -0500

    Fix playRound function prompt

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,17 +7,22 @@ function getComputerChoice () {
     //return the random result
     return choice[random];
 }
+//question string
+let question = 'Rock, Paper, or Scissor?';
 //stored value of getComputerChoice func.
-let computerChoice = getComputerChoice();
+let computerChoice;
+//create variable for getPlayerChoice input prompt
+let playerChoice;
 //both players start at zero
 let userScore = 0;
 let computerScore = 0;
-//create variable for getPlayerChoice input prompt
-let playerChoice = prompt('Rock, Paper, or Scissor?').toLowerCase();
+//start game
+game();
 
 //declare playRound function (two parameters)
 function playRound (playerSelection,computerSelection) {
-    //console.log('rpsGame',computerSelection);
+    playerChoice = prompt(question).toLowerCase();
+    computerChoice = getComputerChoice();
     let result;
     let winner = 'You Win!';
     let loser = 'You Lose!';
@@ -30,49 +35,73 @@ function playRound (playerSelection,computerSelection) {
             if (computerSelection === 'rock')
             {
                 result = 'Draw. Try Again.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             } else if (computerSelection === 'paper')
             {
                 result = `${loser} ${paperWins}`;
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore++} CPU`;
             } else if (computerSelection === 'scissors')
             {
                 result = `${winner} ${rockWins}`;
+                result += '\n';
+                result += `Player: ${userScore++} | ${computerScore} CPU`;
             } else {
                 result = 'not an option... Choose rock, paper or scissors.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             }
             break;
         case 'paper':
             if (computerSelection === 'paper')
             {
                 result = 'Draw. Try Again.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             } else if (computerSelection === 'scissors')
             {
                 result = `${loser} ${scissorsWins}`;
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore++} CPU`;
             } else if (computerSelection === 'rock')
             {
                 result = `${winner} ${paperWins}`;
+                result += '\n';
+                result += `Player: ${userScore++} | ${computerScore} CPU`;
             } else {
                 result = 'not an option... Choose rock, paper or scissors.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             }
             break;
         case 'scissors':
             if (computerSelection === 'scissors')
             {
                 result = 'Draw. Try Again.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             } else if (computerSelection === 'rock')
             {
                 result = `${loser} ${rockWins}`;
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore++} CPU`;
             } else if (computerSelection === 'paper')
             {
                 result = `${winner} ${scissorsWins}`;
+                result += '\n';
+                result += `Player: ${userScore++} | ${computerScore} CPU`;
             } else {
                 result = 'not an option... Choose rock, paper or scissors.';
+                result += '\n';
+                result += `Player: ${userScore} | ${computerScore} CPU`;
             }
             break;
-    } 
+    }
     return result;
 }
 //'GAME OVER' function
-function endGame() {
+function endGame () {
     if (userScore > computerScore) {
         console.log("Game Over! You Win! :)");
     } else if (computerScore > userScore) {
@@ -82,7 +111,13 @@ function endGame() {
 //declare game function
 function game () {
     //call playRound func.
-    console.log(playRound(playerChoice, computerChoice));
     playRound(playerChoice, computerChoice);
-
+    console.log(playRound(playerChoice,computerChoice));
+    //players will continue to play if their scores are lower than 5
+    if (userScore < 5 && computerScore < 5) {
+        game();
+    } else {
+        //'GAME OVER' screen
+        endGame();
+    }
 } 
