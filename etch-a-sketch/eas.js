8c9da28eab8dd99f62821fb73b55c9db910f182d
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-03-28 17:25:50 -0400

    Add div through js
    Create a div in parent 'grid' element.

---
+++ etch-a-sketch/eas.js
@@ -0,0 +1,12 @@
+//get 'grid' div element by ID
+const grid = document.getElementById('grid')
+//create function called createGrid 
+const createGrid = () => {
+    //create a var that makes a child div for 'grid' 
+    const div = document.createElement('div')
+    grid.appendChild(div)
+
+    return
+}
+//call createGrid function
+createGrid()
