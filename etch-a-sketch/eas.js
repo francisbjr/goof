6d0ccab4057ff718ac4764e3e42adff39d39a7c3
JunBug <8799137+francisbjr@users.noreply.github.com>
2023-03-28 17:39:21 -0400

    Make 16 divs of same size

--- etch-a-sketch/eas.js
+++ etch-a-sketch/eas.js
@@ -2,9 +2,12 @@
 const grid = document.getElementById('grid')
 //create function called createGrid 
 const createGrid = () => {
-    //create a var that makes a child div for 'grid' 
-    const div = document.createElement('div')
-    grid.appendChild(div)
+    //loop will run 16 times
+    for (let i = 0; i < 16; i++) {
+        //create a var that makes a child div for 'grid' 
+        let div = document.createElement('div')
+        grid.appendChild(div)
+    }
 
     return
 }
