2874dbed636191f7e31fc2aa89e4426358f8ed76
Francis J.B <8799137+francisbjr@users.noreply.github.com>
2023-06-03 09:46:32 -0400

    Update game grid
    Created nine 3x3 grids, which are inside the game_grid frame. Made new functions to generate boxes and shuffle numbers in each of them. Commented out old labels and the randomInt() as they are  of no use but may be useful in the future.

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,19 +1,52 @@
 import random as r
 import tkinter as tk
-from tkinter import Button, Entry, Label, LabelFrame, Text
+from tkinter import Button, Frame, Label, LabelFrame
 
 root = tk.Tk() # Create root window
 root.title("Sudoku") # Window Title
 root.geometry("400x200") # Window Size
 # root.resizable(0, 0)
 
+########## Functions ##########
+'''
 def randomInt():
     random_number = r.randint(1, 9)
     return random_number
+'''
+
+def create_box():
+    numbers = list(range(1, 10))  # Generate a list of numbers from 1 to 9
+    r.shuffle(numbers)  # Shuffle the numbers randomly
+    box = [numbers[i:i+3] for i in range(0, 9, 3)]  # Divide the shuffled numbers into 3x3 box
+    return box
+
+def create_multiple_boxes(num_boxes):
+    boxes = []
+    while len(boxes) < num_boxes:
+        box = create_box()
+        if box not in boxes:
+            boxes.append(box)
+    return boxes
+
+num_boxes = 9  # Specify the number of boxes you want to create
+boxes = create_multiple_boxes(num_boxes)
 
 # Game Grid
 game_frame = LabelFrame(root, )
 game_frame.grid(row=0, column=0)
+
+    # Print the boxes
+for box in boxes:
+    box_frame = Frame(game_frame, bd=1, relief=tk.RAISED)
+    box_frame.pack(side=tk.LEFT, padx=10, pady=10)
+    for row in box:
+        row_frame = Frame(box_frame)
+        row_frame.pack()
+        for number in row:
+            label = Label(row_frame, text=number, width=4, height=2, relief=tk.RIDGE)
+            label.pack(side=tk.LEFT)
+
+'''
 
 top_left_grid = LabelFrame(game_frame, )
 top_center_grid = LabelFrame(game_frame, )
@@ -34,10 +67,10 @@ middle_right_grid.grid(row=1, column=2)
 bottom_left_grid.grid(row=2, column=0)
 bottom_center_grid.grid(row=2, column=1)
 bottom_right_grid .grid(row=2, column=2)
-
+'''
 # Buttons Frame
 btns_frame = LabelFrame(root, )
-btns_frame.grid(row=0, column=1) # Position for all buttons
+btns_frame.grid(row=1, column=0) # Position for all buttons
 # Buttons (controls padding)
 btn1 = Button(btns_frame, text="1", padx=20 ,pady=15)
 btn2 = Button(btns_frame, text="2", padx=20 ,pady=15)
