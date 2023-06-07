7b73bc2fa908b94df918068b51f18e74152cb447
Francis J.B <8799137+francisbjr@users.noreply.github.com>
2023-06-07 11:09:19 -0400

    Importing new game code
    Working application of sudoku game. Game only shows in the terminal. Working on bringing game to window.

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -1,75 +1,128 @@
-import random as r
+import random
+import math
 import tkinter as tk
 from tkinter import Button, Frame, Label, LabelFrame
 
 root = tk.Tk() # Create root window
 root.title("Sudoku") # Window Title
-root.geometry("800x350") # Window Size
+root.geometry("650x350") # Window Size
 # root.resizable(0, 0)
 
-########## Functions ##########
-'''
-def randomInt():
-    random_number = r.randint(1, 9)
-    return random_number
-'''
-
-def create_box():
-    numbers = list(range(1, 10))  # Generate a list of numbers from 1 to 9
-    r.shuffle(numbers)  # Shuffle the numbers randomly
-    box = [numbers[i:i+3] for i in range(0, 9, 3)]  # Divide the shuffled numbers into 3x3 box
-    return box
-
-def create_multiple_boxes(num_boxes):
-    boxes = []
-    while len(boxes) < num_boxes:
-        box = create_box()
-        if box not in boxes:
-            boxes.append(box)
-    return boxes
-
-# Game Grid
-num_boxes = 1  # Specify the number of boxes you want to create
-boxes = create_multiple_boxes(num_boxes)
+class Sudoku:
+    def __init__(self, N, K):
+        self.N = N
+        self.K = K
+ 
+        # Compute square root of N
+        SRNd = math.sqrt(N)
+        self.SRN = int(SRNd)
+        self.mat = [[0 for _ in range(N)] for _ in range(N)]
+     
+    def fillValues(self):
+        # Fill the diagonal of SRN x SRN matrices
+        self.fillDiagonal()
+ 
+        # Fill remaining blocks
+        self.fillRemaining(0, self.SRN)
+ 
+        # Remove Randomly K digits to make game
+        self.removeKDigits()
+     
+    def fillDiagonal(self):
+        for i in range(0, self.N, self.SRN):
+            self.fillBox(i, i)
+     
+    def unUsedInBox(self, rowStart, colStart, num):
+        for i in range(self.SRN):
+            for j in range(self.SRN):
+                if self.mat[rowStart + i][colStart + j] == num:
+                    return False
+        return True
+     
+    def fillBox(self, row, col):
+        num = 0
+        for i in range(self.SRN):
+            for j in range(self.SRN):
+                while True:
+                    num = self.randomGenerator(self.N)
+                    if self.unUsedInBox(row, col, num):
+                        break
+                self.mat[row + i][col + j] = num
+     
+    def randomGenerator(self, num):
+        return math.floor(random.random() * num + 1)
+     
+    def checkIfSafe(self, i, j, num):
+        return (self.unUsedInRow(i, num) and self.unUsedInCol(j, num) and self.unUsedInBox(i - i % self.SRN, j - j % self.SRN, num))
+     
+    def unUsedInRow(self, i, num):
+        for j in range(self.N):
+            if self.mat[i][j] == num:
+                return False
+        return True
+     
+    def unUsedInCol(self, j, num):
+        for i in range(self.N):
+            if self.mat[i][j] == num:
+                return False
+        return True
+     
+    
+    def fillRemaining(self, i, j):
+        # Check if we have reached the end of the matrix
+        if i == self.N - 1 and j == self.N:
+            return True
+     
+        # Move to the next row if we have reached the end of the current row
+        if j == self.N:
+            i += 1
+            j = 0
+     
+        # Skip cells that are already filled
+        if self.mat[i][j] != 0:
+            return self.fillRemaining(i, j + 1)
+     
+        # Try filling the current cell with a valid value
+        for num in range(1, self.N + 1):
+            if self.checkIfSafe(i, j, num):
+                self.mat[i][j] = num
+                if self.fillRemaining(i, j + 1):
+                    return True
+                self.mat[i][j] = 0
+         
+        # No valid value was found, so backtrack
+        return False
+ 
+    def removeKDigits(self):
+        count = self.K
+ 
+        while (count != 0):
+            i = self.randomGenerator(self.N) - 1
+            j = self.randomGenerator(self.N) - 1
+            if (self.mat[i][j] != 0):
+                count -= 1
+                self.mat[i][j] = 0
+     
+        return
+ 
+    def printSudoku(self):
+        for i in range(self.N):
+            for j in range(self.N):
+                print(self.mat[i][j], end=" ")
+            print()
+ 
+# Driver code
+if __name__ == "__main__":
+    N = 9
+    K = 40
+    sudoku = Sudoku(N, K)
+    sudoku.fillValues()
+    sudoku.printSudoku()
 
 game_frame = LabelFrame(root, )
 game_frame.grid(row=0, column=0) # Position of game board 
 
-    # Print the boxes
-for box in boxes:
-    box_frame = Frame(game_frame, bd=1, relief=tk.RAISED)
-    box_frame.pack(side=tk.LEFT, padx=10, pady=10)
-    for row in range(3):
-     for row in box:
-         row_frame = Frame(box_frame)
-         row_frame.pack()
-         for col in range(3):
-          for number in row:
-              label = Label(row_frame, text=number, width=4, height=2, relief=tk.RIDGE)
-              label.pack(side=tk.LEFT)
 
-'''
-
-top_left_grid = LabelFrame(game_frame, )
-top_center_grid = LabelFrame(game_frame, )
-top_right_grid = LabelFrame(game_frame, )
-middle_left_grid = LabelFrame(game_frame, )
-middle_center_grid = LabelFrame(game_frame, )
-middle_right_grid = LabelFrame(game_frame, )
-bottom_left_grid = LabelFrame(game_frame, )
-bottom_center_grid = LabelFrame(game_frame, )
-bottom_right_grid = LabelFrame(game_frame, )
-
-top_left_grid.grid(row=0, column=0)
-top_center_grid.grid(row=0, column=1)
-top_right_grid.grid(row=0, column=2)
-middle_left_grid.grid(row=1, column=0)
-middle_center_grid.grid(row=1, column=1)
-middle_right_grid.grid(row=1, column=2)
-bottom_left_grid.grid(row=2, column=0)
-bottom_center_grid.grid(row=2, column=1)
-bottom_right_grid .grid(row=2, column=2)
-'''
 # Buttons Frame
 btns_frame = LabelFrame(root, )
 btns_frame.grid(row=0, column=1) # Position for all buttons
